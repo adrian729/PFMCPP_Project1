507af42235b57a83b580480b9c3217656e71d062
adrian729 <adrian.sanchez.albanell@gmail.com>
2023-07-17 08:32:52 +0000

    Added nouns and actions

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ I will request a change be made to one of your 10 nouns for the sole purpose of 
     Choose nouns that do things.  
     avoid choosing nouns that have things done TO them.
     If your noun consists of multiple words, use camelCaseToNameIt.
+
  
  2) for each of the 10 nouns, write out 3 actions it might perform, in plain english.
     a) AVOID state-checking actions, like "Car has a horn".  i.e.
@@ -32,6 +33,7 @@ I will request a change be made to one of your 10 nouns for the sole purpose of 
             other actions that would work:
                 action: consume electricity
                 action: burn out and destroy filament.
+
 
         
  3) write out how you'd call that action in pseudo code, in the space after the plain-english action
@@ -52,85 +54,85 @@ arm.rotateCounterClockwise();           // 4) demonstrates camelCase
 
 
 //  1)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: toaster
+//  action 1: burn bread
+toaster.burn()
+//  action 2: ping (make sound)
+toaster.ping()
+//  action 3: throw bread
+toaster.throw()
 //  2)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: keyboard
+//  action 1: send key pressed signal
+keyboard.sendKeySignal()
+//  action 2: illuminate option led
+keyboard.illuminateLed()
+//  action 3: connect via bluetooth/usb
+keyboard.connect()
 //  3)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: mouse
+//  action 1: detect coordinates change
+mouse.detectCoordinatesChange()
+//  action 2: send click/movement signal
+mouse.sendSignal()
+//  action 3: connect via bluetooth/usb
+mouse.connect()
 //  4)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: clock
+//  action 1: move hand
+clock.moveHand()
+//  action 2: annoy with alarm
+clock.alarm()
+//  action 3: make tick sound
+clock.tick()
 //  5)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: computer screen
+//  action 1: paint image on itself
+screen.paint()
+//  action 2: show selected input (example: screen with 2 hdmi inputs)
+screen.showSelectedInput()
+//  action 3: illuminate more (more brightness)
+screen.illuminateMore()
 //  6)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: soldering station
+//  action 1: heat tip
+solderingStation.heat()
+//  action 2: show temperature
+solderingStation.showTemperature()
+//  action 3: consume power
+solderingStation.consumePower()
 //  7)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: audio interface
+//  action 1: send sound to headphones
+audioInterface.sendSoundToHeadphones()
+//  action 2: process sound from input
+audioInterface.processInputSound()
+//  action 3: illuminate gain leds
+audioInterface.illuminateGain()
 //  8)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: Boss BD-2
+//  action 1: change sound level
+bd2.changeLevel()
+//  action 2: change sound tone
+bd2.changeTone()
+//  action 3: change sound gain
+bd2.changeGain()
 //  9)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: fridge
+//  action 1: cool compartment A
+fridge.coolCompartmentA()
+//  action 2: freeze compartment B
+fridge.freezeCompartmentB()
+//  action 3: illuminate compartment
+fridge.illuminate()
 //  10)
-//  Noun:
-//  action 1:
-
-//  action 2:
-
-//  action 3:
-
+//  Noun: shower
+//  action 1: spray burning water
+shower.sprayBurningWater()
+//  action 2: spray frozen water
+shower.sprayFrozenWater()
+//  action 3: spray water from wrong head
+shower.throwWaterFromHead()
 
 
 #endif
